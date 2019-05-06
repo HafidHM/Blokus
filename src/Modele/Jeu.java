01dbd301ea3b620ebdc7fc33a815cb60e4451424
Tom Priore <azertkiller@gmail.com>
2019-05-06 17:53:24 +0000

    Replace Jeu.java

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -4,91 +4,70 @@ import Patterns.Observable;
 
 public class Jeu extends Observable {
 	boolean enCours;
-	public int[][] plateau; // Mis en public pour pouvoir y acceder depuis JoueurIA
-	int libre;
 	public int joueurCourant; // Mis en public pour pouvoir y acceder depuis JoueurIA
-
-	public Jeu(int m,int n) {
-		plateau = new int[m][n];
-		libre = m*n;
+	boolean [][] pieceJoueurs = new boolean[4][21]; // 4 joueurs max et 21 pièces au total
+	public Plateau plateau;
+			
+	public Jeu(int n) {
+		plateau = new Plateau(n);
 		enCours = true;
-		for (int i = 0; i < plateau.length; i++)
-			for (int j = 0; j < plateau[0].length; j++)
-				plateau[i][j] = -1;
-		joueurCourant = 0;
+		for (int i = 0; i < plateau.p.length; i++)
+			for (int j = 0; j < plateau.p[0].length; j++)
+				plateau.newVal(i, j, 0);
+		joueurCourant = 1;
 	}
 
-	public void jouer(int l, int c) {
-		if (enCours && (plateau[l][c] == -1) ) {
-			//plateau[l][c] = joueurCourant;
-			//libre--;
-			/*boolean vertical = true, horizontal = true, slash = true, antiSlash = true;
-			for (int p = 0; p < plateau.length; p++) {
-				horizontal = horizontal && (plateau[l][p] == joueurCourant);
-				vertical = vertical && (plateau[p][c] == joueurCourant);
-				slash = slash && (plateau[p][p] == joueurCourant);
-				antiSlash = antiSlash && (plateau[p][plateau.length - p - 1] == joueurCourant);
-			}*/
-			for (int i = 0 ;i<hauteur();i++){
-				for(int j =0;j<largeur();j++){
-					if(i>=l && j>=c && libre(i,j)){
-						plateau[i][j] = 1;
-						libre--;
-					}
+	public boolean jouer(int l, int c, Piece piece) {
+
+		for(int i=l;i<piece.taille+l;i++) {
+			for (int j = c; j < piece.taille+c; j++) {
+				if((i>=plateau.p.length || j>=plateau.p.length) && (piece.carres[i-l][j-c])){
+					return false;
+				} else if ((i<plateau.p.length && j<plateau.p.length) && (plateau.valeur(i, j) != 0)){
+					return false;
 				}
 			}
-			//enCours = !(horizontal || vertical || slash || antiSlash) && (libre > 0);
-			enCours = !coupPoison(l, c);
-			joueurCourant = 1 - joueurCourant;
-			if(enCours==false){
-				System.out.println("Fin du jeu, joueur gagnant : "+joueurCourant);
-			}
-
-			metAJour();
 		}
-	}
 
-	public boolean libre(int i, int j) {
-		return valeur(i, j) == -1;
-	}
+		for(int i=l;i<piece.taille+l;i++) {
+			for (int j = c; j < piece.taille+c; j++) {
+				if(piece.carres[i-l][j-c]){
+					plateau.newVal(i, j, joueurCourant);
+				}
+			}
+		}
 
-	public int valeur(int i, int j) {
-		return plateau[i][j];
+		for(int i=0;i<plateau.p.length;i++) {
+			for (int j = 0; j < plateau.p.length; j++) {
+				System.out.print(plateau.valeur(i, j));
+			}
+			System.out.println();
+		}
+
+		updateJoueurCour();
+		metAJour();
+
+		return true;
+
 	}
 
 	public boolean enCours() {
 		return enCours;
 	}
 
-	public int largeur() {
-		return plateau[0].length;
-	}
 
-	public int hauteur() {
-		return plateau.length;
-	}
-
-	public boolean coupPoison(int i, int j) {
-		return (i==0 && j ==0) ;
-	}
-
-	public boolean nonPerdrePossible(){
-		return libre!=1;
-	}
-
-
-	public boolean estFeuille(){
-		return libre==0;
-	}
-	
     public void refaire() {
-		plateau = new int[5][7];
-		libre = 5*7;
+		plateau.p = new int[6][6];
 		enCours = true;
-		for (int i = 0; i < plateau.length; i++)
-			for (int j = 0; j < plateau[0].length; j++)
-				plateau[i][j] = -1;
-		joueurCourant = 0;
+		for (int i = 0; i < plateau.p.length; i++)
+			for (int j = 0; j < plateau.p[0].length; j++)
+				plateau.newVal(i, j, 0);
+		joueurCourant = 1;
         metAJour();
    }
+
+   public void updateJoueurCour(){
+		joueurCourant = ((joueurCourant+1) %4);
+   }
+
 }
