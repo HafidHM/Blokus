3e06ca2253cb0e80b097acca35f1529aa9d8df96
Tom Priore <azertkiller@gmail.com>
2019-05-07 07:31:01 +0000

    Implantation temporaire des 4 joueurs

--- src/Controleur/ControleurMediateur.java
+++ src/Controleur/ControleurMediateur.java
@@ -1,32 +1,5 @@
 package Controleur;
 
-/*
- * Morpion pédagogique
-
- * Copyright (C) 2016 Guillaume Huard
-
- * Ce programme est libre, vous pouvez le redistribuer et/ou le
- * modifier selon les termes de la Licence Publique Générale GNU publiée par la
- * Free Software Foundation (version 2 ou bien toute autre version ultérieure
- * choisie par vous).
-
- * Ce programme est distribué car potentiellement utile, mais SANS
- * AUCUNE GARANTIE, ni explicite ni implicite, y compris les garanties de
- * commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
- * Licence Publique Générale GNU pour plus de détails.
-
- * Vous devez avoir reçu une copie de la Licence Publique Générale
- * GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
- * États-Unis.
-
- * Contact: Guillaume.Huard@imag.fr
- *          Laboratoire LIG
- *          700 avenue centrale
- *          Domaine universitaire
- *          38401 Saint Martin d'Hères
- */
-
 import Modele.Jeu;
 import Vue.FenetreGraphique;
 
@@ -34,23 +7,25 @@ public class ControleurMediateur {
 	Jeu jeu;
 	FenetreGraphique f;
 	Joueur[] joueurs;
-        boolean jeuAutomatique;
+	boolean jeuAutomatique;
 	int joueurCourant;
 	final int lenteurAttente = 50;
 	int decompte;
-        String niv;
+	String niv;
 	public ControleurMediateur(Jeu j, FenetreGraphique fen) {
 		jeu = j;
 		f = fen;
-                jeuAutomatique = false;
-                f.changeBoutonIA(jeuAutomatique);
-		joueurs = new Joueur[2];
-                joueurs[0] = new JoueurHumain(0, jeu);
+		jeuAutomatique = false;
+		f.changeBoutonIA(jeuAutomatique);
+		joueurs = new Joueur[4];
+		joueurs[0] = new JoueurHumain(1, jeu);
 		if (jeuAutomatique)
-			joueurs[1] = new JoueurIA(1, jeu);
+			joueurs[1] = new JoueurIA(2, jeu);
 		else
-			joueurs[1] = new JoueurHumain(1, jeu);
-                niv = f.ecouteurNiveau();      
+			joueurs[1] = new JoueurHumain(2, jeu);
+			niv = f.ecouteurNiveau();
+			joueurs[2] = new JoueurHumain(3, jeu);
+			joueurs[3] = new JoueurHumain(4, jeu);
 	}
 
 	public void redimensionnement() {
@@ -70,19 +45,20 @@ public class ControleurMediateur {
 		decompte = lenteurAttente;
 	}
 
-        public boolean choisirNiveau(String niveau){
+	public boolean choisirNiveau(String niveau){
             if(!niv.equals(niveau)){
                 System.out.println("Vous avez choisit " + niveau);
                 niv=niveau;
             } 
             switch(niveau){
                 case "Easy":return joueurs[joueurCourant].tempsEcoule();
-                case "Medium":return joueurs[joueurCourant].tempsEcouleNonPerdant();
-                case "Hard":return joueurs[joueurCourant].tempsEcouleMinimax();
+                //case "Medium":return joueurs[joueurCourant].tempsEcouleNonPerdant();
+                //case "Hard":return joueurs[joueurCourant].tempsEcouleMinimax();
                 default:return false;
             }
         }
-        public void basculeIA(boolean value) {
+
+    public void basculeIA(boolean value) {
 		jeuAutomatique = value;
 		System.out.println("jeuautomatique " + jeuAutomatique);
 		f.changeBoutonIA(value);
