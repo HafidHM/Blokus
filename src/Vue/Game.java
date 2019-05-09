2c36d292529c93a8596d9f145c409383de183cc9
biyun233 <syviass@outlook.com>
2019-05-09 07:51:51 +0000

    Replace Game.java

--- src/Vue/Game.java
+++ src/Vue/Game.java
@@ -1,11 +1,7 @@
-package blokus.Vue;
+package Vue;
 
-import blokus.Modele.Jeu;
-import blokus.Vue.App;
-import blokus.test.view.Aide;
-import blokus.test.view.Jouer;
-import blokus.test.view.Menu;
-import blokus.test.view.Parametre;
+import Modele.Jeu;
+import Vue.App;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,14 +10,15 @@ public class Game extends Application{
 	Jeu jeu;
 	
 	public void onLaunch() {
+		
 		app.setTitle("Test Game");
 		app.setWidth(800);
 		app.setHeight(600);
 		
-		app.regView("Menu", new Menu());
-		app.regView("Aide", new Aide());
-		app.regView("Parametre", new Parametre());
-		app.regView("Jouer", new Jouer());
+		app.regView("Menu", new ViewMenu(jeu));
+		app.regView("Aide", new ViewAide(jeu));
+		app.regView("Parametre", new ViewParametre(jeu));
+		app.regView("Jouer", new ViewJouer(jeu));
 		app.gotoView("Menu");
 	};
 	
@@ -35,8 +32,9 @@ public class Game extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		app = new App(primaryStage);
+		jeu = new Jeu(20,20);
+		app = new App(jeu,primaryStage);
+		
 		app.onLaunch = this::onLaunch;
 		app.onFinish = this::onFinish;
 		app.onExit= this::onExit;
