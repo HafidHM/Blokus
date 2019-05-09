1686756529d477a16f64c846ec955b6909a50705
biyun233 <syviass@outlook.com>
2019-05-09 07:50:37 +0000

    Replace Jouer.java

--- src/Vue/Jouer.java
+++ src/Vue/Jouer.java
@@ -1,47 +1,77 @@
-package blokus.Vue;
+package Vue;
 import static blokus.Framework.app;
 
-import javafx.geometry.Pos;
+import Modele.Jeu;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 
-public class Jouer extends View {
+public class ViewJouer extends View {
+	public ViewJouer(Jeu j) {
+		super(j);
+	}
+
 	private Button quitBtn;
-	private Pane vue;
+	private Canvas canvas;
+	private StackPane pane;
+	
 	@Override
 	public void onLaunch() {	
-		vue = getPane();
-		VBox boiteTexte = new VBox();
-        boiteTexte.setAlignment(Pos.CENTER);
-                     
-        Label titre = new Label("Gaufre");
-        titre.setMaxHeight(Double.MAX_VALUE);
-        titre.setAlignment(Pos.TOP_CENTER);
-        boiteTexte.getChildren().add(titre);
-        VBox.setVgrow(titre, Priority.ALWAYS);
-
+		setPrefSize(800, 600);
+		canvas = new Canvas(400,400);
+		pane = new StackPane(canvas);
         quitBtn = new Button("Quit");
         quitBtn.setOnAction((event)->{
 			app.exit();
 		}); 
-        boiteTexte.getChildren().add(quitBtn);
-        Label copyright = new Label("Copyright G. Huard, 2018");
-        copyright.setMaxHeight(Double.MAX_VALUE);
-        copyright.setAlignment(Pos.BOTTOM_LEFT);
-        boiteTexte.getChildren().add(copyright);
-        VBox.setVgrow(copyright, Priority.ALWAYS);
-        boiteTexte.setAlignment(Pos.BOTTOM_RIGHT);
-        getChildren().add(boiteTexte);
-        
-        /*HBox boiteScene = new HBox();
-        boiteScene.getChildren().add(vue);
-		boiteScene.getChildren().add(boiteTexte);
-		getChildren().add(boiteScene);*/
+
+        /*setTopAnchor(pane, hauteurCase);
+        setLeftAnchor(pane, hauteurCase);
+        setLeftAnchor(quitBtn,750);
+        setTopAnchor(quitBtn, 550);*/
+        getChildren().add(pane);
+        getChildren().add(quitBtn);
+        miseAJour();
+	}
+	
+	@Override
+	public void miseAJour() {
+        double lignes = jeu.hauteur();
+        double colonnes = jeu.largeur();
+        largeurCase = largeur() / colonnes;
+        hauteurCase = hauteur() / lignes;
+        largeurCase = Math.max(largeurCase,hauteurCase);
+        hauteurCase = Math.max(largeurCase,hauteurCase);
+        System.out.println("lig = " + lignes + "col = " + colonnes);
+        System.out.println("lar = " + largeur() + "hau = " + hauteur());
+        System.out.println("ligC = " + largeurCase+ "colC = " + hauteurCase);
+        GraphicsContext g = canvas.getGraphicsContext2D();
+        g.clearRect(0, 0, largeur(), hauteur());
+        // Grille
+        for (int i=0; i<lignes+1;i++) {
+            g.strokeLine(0, i*hauteurCase, largeur(), i*hauteurCase);
+        }
+        for (int i=0; i<colonnes+1;i++) {
+            g.strokeLine(i*largeurCase, 0, i*largeurCase, hauteur());
+        }
+
+	}
+	
+	double largeur() {
+		return canvas.getWidth();
 	}
 
+	double hauteur() {
+		return canvas.getHeight();
+	}
 
+	public double largeurCase() {
+		return largeurCase;
+	}
+
+	public double hauteurCase() {
+		return hauteurCase;
+	}
+	
 }
