d176b60b6dfbf4122ab6f16d419ade3631aada71
Tom Priore <azertkiller@gmail.com>
2019-05-09 07:55:26 +0000

    MAJ

--- src/InterfaceGraphique.java
+++ src/InterfaceGraphique.java
@@ -1,33 +1,3 @@
-/*
- * Morpion pédagogique
-
- * Copyright (C) 2016 Guillaume Huard
-
- * Ce programme est libre, vous pouvez le redistribuer et/ou le
- * modifier selon les termes de la Licence Publique Générale GNU publiée par la
- * Free Software Foundation (version 2 ou bien toute autre version ultérieure
- * choisie par vous).
-
- * Ce programme est distribué car potentiellement utile, mais SANS
- * AUCUNE GARANTIE, ni explicite ni implicite, y compris les garanties de
- * commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
- * Licence Publique Générale GNU pour plus de détails.
-
- * Vous devez avoir reçu une copie de la Licence Publique Générale
- * GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
- * États-Unis.
-
- * Contact: Guillaume.Huard@imag.fr
- *          Laboratoire LIG
- *          700 avenue centrale
- *          Domaine universitaire
- *          38401 Saint Martin d'Hères
- */
-
-import java.security.InvalidParameterException;
-import java.util.Iterator;
-
 import Controleur.ControleurMediateur;
 import Modele.Jeu;
 import Vue.FenetreGraphique;
@@ -47,7 +17,7 @@ public class InterfaceGraphique extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		jeu = new Jeu(5,7);
+		jeu = new Jeu(23);
 		FenetreGraphique f = new FenetreGraphique(jeu, primaryStage);
 		ControleurMediateur c = new ControleurMediateur(jeu, f);
                 f.ecouteurIA(new EventHandler<ActionEvent>() {
