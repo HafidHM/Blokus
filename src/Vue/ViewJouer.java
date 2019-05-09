39aae181f674d002a3f0a94d6bb602be3f4fa6d7
biyun233 <syviass@outlook.com>
2019-05-09 14:31:28 +0000

    Replace ViewJouer.java

--- src/Vue/ViewJouer.java
+++ src/Vue/ViewJouer.java
@@ -1,63 +1,127 @@
 package Vue;
 import static blokus.Framework.app;
 
+import Controleur.ControleurMediateur;
 import Modele.Jeu;
-import Modele.Plateau;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
 public class ViewJouer extends View {
-	public ViewJouer(Jeu j, Plateau plateau) {
-		super(j,plateau);
+	
+	public ViewJouer(Jeu j) {
+		super(j);
 	}
 
 	private Button quitBtn;
-	private Canvas canvas;
-	private StackPane pane;
+	private Canvas canPlateau;
+	private Canvas canPiece;
+	private Canvas canAffiche;
+	private AnchorPane panePlateau;
+	private AnchorPane panePiece;
+	private AnchorPane paneAffiche;
 	
 	@Override
 	public void onLaunch() {	
-		setPrefSize(900, 800);
-		canvas = new Canvas(400,400);
-		pane = new StackPane(canvas);
+		ControleurMediateur c = new ControleurMediateur(jeu,this);
+		canPlateau = new Canvas(450,400);
+		panePlateau = new AnchorPane(canPlateau);
+		
         quitBtn = new Button("Quit");
+        quitBtn.setLayoutX(150);
+        quitBtn.setLayoutY(550);
         quitBtn.setOnAction((event)->{
 			app.exit();
 		}); 
+        
+        canPiece = new Canvas(560,180);
+        panePiece = new AnchorPane(canPiece);
+        
+        canAffiche = new Canvas(200,200);
+        paneAffiche = new AnchorPane(canAffiche);
+        paneAffiche.setLayoutY(100);
+        
+        Group gPlateau = new Group();
+        Group gPiece = new Group();
+        Group gAffiche = new Group();
+        
 
-        /*setTopAnchor(pane, hauteurCase);
-        setLeftAnchor(pane, hauteurCase);
-        setLeftAnchor(quitBtn,750);
-        setTopAnchor(quitBtn, 550);*/
-        getChildren().add(pane);
-        getChildren().add(quitBtn);
+        gPlateau.getChildren().add(panePlateau);
+        gAffiche.getChildren().addAll(quitBtn, paneAffiche);
+        gPiece.getChildren().add(panePiece);
+   
+       
+        
+        AnchorPane.setTopAnchor(gPlateau, (double) 10);
+        AnchorPane.setLeftAnchor(gPlateau, (double) 20);
+
+        
+        AnchorPane.setTopAnchor(gPiece, 430.0);
+        AnchorPane.setLeftAnchor(gPiece, 10.0);
+        AnchorPane.setBottomAnchor(gPiece, 10.0);
+
+       // AnchorPane.setTopAnchor(gAffiche, 10.0);
+        AnchorPane.setLeftAnchor(gAffiche, 480.0);
+        AnchorPane.setRightAnchor(gAffiche, 10.0);
+        AnchorPane.setBottomAnchor(gAffiche, 10.0);
+        
+        getChildren().addAll(gPlateau,gPiece,gAffiche);
+        
+        
+		
+        jeu.ajouteObservateur(this);
         miseAJour();
+        
+        canPlateau.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent e) {
+				c.clicSouris(e.getX(), e.getY());
+				System.out.println("X = " + e.getX() + " " + " Y = " + e.getY());
+			}
+		});
+        
+        
+        
+        new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				c.tictac();
+			}
+		}.start();
 	}
+	
+	
+
 	
 	@Override 
         public void miseAJour() {
             double lignes = plateau.taille();
             double colonnes = plateau.taille();
-            largeurCase = largeur() / colonnes;
-            hauteurCase = hauteur() / lignes;
-            GraphicsContext g = canvas.getGraphicsContext2D();
-            g.clearRect(0, 0, largeur(), hauteur());
+            largeurCase = largeurPlateau() / colonnes;
+            hauteurCase = hauteurPlateau() / lignes;
+
+            GraphicsContext gPlateau = canPlateau.getGraphicsContext2D();
+            gPlateau.clearRect(0, 0, largeurPlateau(), hauteurPlateau());
             // Grille
             for (int i=0; i<lignes;i++) {
                 for (int j=0; j<colonnes;j++) {
-                    g.setFill(Color.LIGHTGRAY);
-                    g.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
+                    gPlateau.setFill(Color.LIGHTGRAY);
+                    gPlateau.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
 
                 }
             }
-            for (int i=1; i<lignes;i++) {
-                g.strokeLine(0, i*hauteurCase, largeur(), i*hauteurCase);
+            for (int i=0; i<lignes+1;i++) {
+                gPlateau.strokeLine(0, i*hauteurCase, largeurPlateau(), i*hauteurCase);
             }
-            for (int i=1; i<colonnes+1;i++) {
-                g.strokeLine(i*largeurCase, 0, i*largeurCase, hauteur());
+            for (int i=0; i<colonnes+1;i++) {
+                gPlateau.strokeLine(i*largeurCase, 0, i*largeurCase, hauteurPlateau());
             }
             // Coups
             for (int i=0; i<lignes; i++)
@@ -66,44 +130,81 @@ public class ViewJouer extends View {
                         case 0:
                             break;
                         case 1:
-                            g.setFill(Color.DARKOLIVEGREEN);
-                            g.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
+                            gPlateau.setFill(Color.DARKOLIVEGREEN);
+                            gPlateau.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
                             break;
                         case 2:
-                            g.setFill(Color.DARKSLATEBLUE);
-                            g.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
+                            gPlateau.setFill(Color.DARKSLATEBLUE);
+                            gPlateau.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
                             break;
                         case 3:
-                            g.setFill(Color.YELLOW);
-                            g.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
+                            gPlateau.setFill(Color.YELLOW);
+                            gPlateau.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
                             break;
                         case 4:
-                            g.setFill(Color.INDIANRED);
-                            g.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
+                            gPlateau.setFill(Color.INDIANRED);
+                            gPlateau.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
                             break;
                         case 8:
-                            g.setFill(Color.LIGHTGREEN);
-                            g.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
+                            gPlateau.setFill(Color.LIGHTGREEN);
+                            gPlateau.fillRect(j*largeurCase, i*hauteurCase, largeurCase, hauteurCase);
                             break;
                     }
+            
+            //canPiece
+            GraphicsContext gPiece = canPiece.getGraphicsContext2D();
+            gPiece.clearRect(0, 0, largeurPiece(), hauteurPiece());
+           
+            gPiece.strokeLine(0, 0, 0, hauteurPiece());
+            gPiece.strokeLine(0, 0, largeurPiece(), 0);
+            gPiece.strokeLine(largeurPiece(), 0, largeurPiece(), hauteurPiece());
+            gPiece.strokeLine(0, hauteurPiece(), largeurPiece(), hauteurPiece());
+
+            //canAffiche
+            GraphicsContext gAffiche = canAffiche.getGraphicsContext2D();
+            gAffiche.clearRect(0, 0, largeurPlateau(), hauteurPlateau());
+            
+            gAffiche.strokeLine(0, 0, 0, hauteurAffiche());
+            gAffiche.strokeLine(0, 0, largeurAffiche(), 0);
+            gAffiche.strokeLine(largeurAffiche(), 0, largeurAffiche(), hauteurAffiche());
+            gAffiche.strokeLine(0, hauteurAffiche(), largeurAffiche(), hauteurAffiche());
 	}
 
 
 	
-	double largeur() {
-		return canvas.getWidth();
+	double largeurPlateau() {
+		return canPlateau.getWidth();
 	}
 
-	double hauteur() {
-		return canvas.getHeight();
+	double hauteurPlateau() {
+		return canPlateau.getHeight();
+	}
+	
+	double largeurPiece() {
+		return canPiece.getWidth();
+	}
+
+	double hauteurPiece() {
+		return canPiece.getHeight();
+	}
+	
+	double largeurAffiche() {
+		return canAffiche.getWidth();
+	}
+
+	double hauteurAffiche() {
+		return canAffiche.getHeight();
 	}
 
 	public double largeurCase() {
+		
 		return largeurCase;
 	}
 
 	public double hauteurCase() {
+		
 		return hauteurCase;
 	}
+
 	
 }
