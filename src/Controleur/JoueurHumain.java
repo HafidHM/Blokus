6298428bc2feb236a8a67b480d0828338cd27208
Tom Priore <azertkiller@gmail.com>
2019-05-09 07:54:37 +0000

    Replace JoueurHumain.java

--- src/Controleur/JoueurHumain.java
+++ src/Controleur/JoueurHumain.java
@@ -1,50 +1,41 @@
 package Controleur;
-/*
- * Morpion pédagogique
- * Copyright (C) 2016 Guillaume Huard
-
- * Ce programme est libre, vous pouvez le redistribuer et/ou le
- * modifier selon les termes de la Licence Publique Générale GNU publiée par la
- * Free Software Foundation (version 2 ou bien toute autre version ultérieure
- * choisie par vous).
-
- * Ce programme est distribué car potentiellement utile, mais SANS
- * AUCUNE GARANTIE, ni explicite ni implicite, y compris les garanties de
- * commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
- * Licence Publique Générale GNU pour plus de détails.
-
- * Vous devez avoir reçu une copie de la Licence Publique Générale
- * GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
- * États-Unis.
-
- * Contact: Guillaume.Huard@imag.fr
- *          Laboratoire LIG
- *          700 avenue centrale
- *          Domaine universitaire
- *          38401 Saint Martin d'Hères
- */
 
 import Modele.Jeu;
+import Modele.Piece;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 
 class JoueurHumain extends Joueur {
+	Random r;
 	JoueurHumain(int n, Jeu p) {
 		super(n, p);
+		r = new Random();
 	}
 
 	@Override
 	boolean jeu(int i, int j) {
-		// A adapter selon le jeu,
-		// Un coup peut être constitué de plusieurs passages par cette fonction, ex :
-		// - selection d'un pièce + surlignage des coups possibles
-		// - selection de la destination
-		// Autrement dit une machine à état peut aussi être gérée par un objet de cette
-		// classe. Dans le cas du morpion, un clic suffit.
-		if (plateau.libre(i, j)) {
-			plateau.jouer(i, j);
+
+		if (plateau.plateau.libre(i, j)) {
+			int num = r.nextInt(3);
+			//plateau.plateau.availableCases(plateau.joueurCourant);
+
+			/*try {
+				Scanner keyboard = new Scanner(System.in);
+				System.out.println("Entrez un entier");
+				while(num < 1 || num > 21) {
+					num = keyboard.nextInt();
+				}
+			} catch (InputMismatchException e){
+				System.err.println("Entrez un entier !!!");
+			}*/
+			plateau.jouer(i, j, plateau.choixPiece(num));
+
 			return true;
 		} else {
 			return false;
 		}
 	}
+
 }
