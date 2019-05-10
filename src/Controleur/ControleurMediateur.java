fc925d40196be11322c775abc947d8b7fde9f84e
biyun233 <syviass@outlook.com>
2019-05-10 16:27:57 +0000

    Replace ControleurMediateur.java

--- src/Controleur/ControleurMediateur.java
+++ src/Controleur/ControleurMediateur.java
@@ -5,28 +5,26 @@ import Vue.ViewJouer;
 
 public class ControleurMediateur {
 	Jeu jeu;
-    ViewJouer vjouer;
-	Joueur[] joueurs;
-	boolean jeuAutomatique;
+	ViewJouer vjouer;
+	JoueurHumain[] joueurs;
+    boolean jeuAutomatique;
 	int joueurCourant;
 	final int lenteurAttente = 50;
 	int decompte;
-	String niv;
+    String niv;
 	public ControleurMediateur(Jeu j, ViewJouer v) {
 		jeu = j;
 		vjouer = v;
-		jeuAutomatique = false;
-		joueurs = new Joueur[4];
-		joueurs[0] = new JoueurHumain(1, jeu);
+        jeuAutomatique = false;
+		joueurs = new JoueurHumain[2];
+        joueurs[0] = new JoueurHumain(0, jeu);
 		if (jeuAutomatique)
-			joueurs[1] = new JoueurIA(2, jeu);
+			joueurs[1] = new JoueurIA(1, jeu);
 		else
-			joueurs[1] = new JoueurHumain(2, jeu);
-			joueurs[2] = new JoueurHumain(3, jeu);
-			joueurs[3] = new JoueurHumain(4, jeu);
+			joueurs[1] = new JoueurHumain(1, jeu);   
 	}
 
-public void redimensionnement() {
+	public void redimensionnement() {
 		vjouer.miseAJour();
 	}
 
@@ -43,50 +41,44 @@ public void redimensionnement() {
 		decompte = lenteurAttente;
 	}
 
-	public boolean choisirNiveau(String niveau){
+       /* public boolean choisirNiveau(String niveau){
             if(!niv.equals(niveau)){
                 System.out.println("Vous avez choisit " + niveau);
                 niv=niveau;
             } 
             switch(niveau){
                 case "Easy":return joueurs[joueurCourant].tempsEcoule();
-                //case "Medium":return joueurs[joueurCourant].tempsEcouleNonPerdant();
-                //case "Hard":return joueurs[joueurCourant].tempsEcouleMinimax();
+                case "Medium":return joueurs[joueurCourant].tempsEcouleNonPertant();
+                case "Hard": return joueurs[joueurCourant].tempsEcouleMinimax();
                 default:return false;
             }
+        }*/
+        public void basculeIA(boolean value) {
+        	jeuAutomatique = value;
+        	System.out.println("jeuautomatique " + jeuAutomatique);
+        	//f.changeBoutonIA(value);
+            if (jeuAutomatique)
+            	joueurs[1] = new JoueurIA(1, jeu);
+            else
+            	joueurs[1] = new JoueurHumain(1, jeu);
         }
-
-    public void basculeIA(boolean value) {
-		jeuAutomatique = value;
-		System.out.println("jeuautomatique " + jeuAutomatique);
-		//f.changeBoutonIA(value);
-                if (jeuAutomatique)
-			joueurs[1] = new JoueurIA(1, jeu);
-		else
-			joueurs[1] = new JoueurHumain(1, jeu);
-	}
         
-	public void tictac() {
-		if (jeu.enCours()) {
-			if (decompte == 0) {
-				//if (choisirNiveau(f.ecouteurNiveau())) {
-				if(joueurs[joueurCourant].tempsEcoule()){
-					changeJoueur();
+        public void tictac() {
+			if (jeu.enCours()) {
+				if (decompte == 0) {
+					//if (choisirNiveau(f.ecouteurNiveau())) {
+					if(joueurs[joueurCourant].tempsEcoule())
+						changeJoueur();
 				} else {
-					System.out.println("On vous attend, joueur " + joueurs[joueurCourant].num());
-					decompte = lenteurAttente;
-                                        
+						//System.out.println("On vous attend, joueur " + joueurs[joueurCourant].num());
+						decompte = lenteurAttente;
+	                                        
 				}
 			} else {
-				decompte--;
-			}
-                       
+					decompte--;
+				}          
 		}
             
 	}
-	
-    public void refaire() {
-            jeu.refaire();
-	}
         
-}
+
