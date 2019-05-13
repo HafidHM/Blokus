11ccb8df4ab8f88433f3ed0075e00499e0065fc7
Tom Priore <azertkiller@gmail.com>
2019-05-13 13:47:51 +0000

    Joueur-1 est plus Joueur-1 mais Joueur

--- src/Controleur/ControleurMediateur.java
+++ src/Controleur/ControleurMediateur.java
@@ -7,15 +7,15 @@ public class ControleurMediateur {
 	Jeu jeu;
 	ViewJouer vjouer;
 	Joueur[] joueurs;
-    boolean jeuAutomatique;
+	boolean jeuAutomatique;
 	int joueurCourant;
 	final int lenteurAttente = 50;
 	int decompte;
-    String niv;
+	String niv;
 	public ControleurMediateur(Jeu j, ViewJouer v) {
 		jeu = j;
 		vjouer = v;
-        jeuAutomatique = false;
+		jeuAutomatique = false;
 		joueurs = new Joueur[4];
 		joueurs[0] = new JoueurHumain(0, jeu);
 		niv = "Easy";
@@ -28,7 +28,7 @@ public class ControleurMediateur {
 			joueurs[2] = new JoueurHumain(2, jeu);
 			joueurs[3] = new JoueurHumain(3, jeu);
 		}
-  
+
 	}
 
 	public void redimensionnement() {
@@ -38,15 +38,15 @@ public class ControleurMediateur {
 	public void clicSouris(double x, double y) {
 		int l = (int) (y / vjouer.hauteurCase());
 		int c = (int) (x / vjouer.largeurCase());
-		
+
 		if (joueurs[joueurCourant].jeu(l, c))
 			changeJoueur();
 	}
-	
+
 	public void selectPiece(double x, double y) {
 		int l = (int) (y / vjouer.hauteurCasePiece());
 		int c = (int) (x / vjouer.largeurCasePiece());
-		
+
 		jeu.plateauAffiche.PlacerPiece(jeu.choixPiece(jeu.plateauPiece.valeur(l,c)));
 		System.out.println("valeur est " +jeu.plateauPiece.valeur(l,c));
 		jeu.setSelected(jeu.plateauPiece.valeur(l,c));
@@ -57,51 +57,51 @@ public class ControleurMediateur {
 		jeu.plateauAffiche.initPlateauAffiche();
 		vjouer.miseAJour();
 	}
-	
+
 
 	void changeJoueur() {
 		joueurCourant = (joueurCourant + 1) % joueurs.length;
 		decompte = lenteurAttente;
 	}
 
-       /* public boolean choisirNiveau(String niveau){
-            if(!niv.equals(niveau)){
-                System.out.println("Vous avez choisit " + niveau);
-                niv=niveau;
-            } 
-            switch(niveau){
-                case "Easy":return joueurs[joueurCourant].tempsEcoule();
-                case "Medium":return joueurs[joueurCourant].tempsEcouleNonPertant();
-                case "Hard": return joueurs[joueurCourant].tempsEcouleMinimax();
-                default:return false;
-            }
-        }*/
-        public void basculeIA(boolean value) {
-        	jeuAutomatique = value;
-        	System.out.println("jeuautomatique " + jeuAutomatique);
-        	//f.changeBoutonIA(value);
-            if (jeuAutomatique)
-            	joueurs[1] = new JoueurIA(1, jeu);
-            else
-            	joueurs[1] = new JoueurHumain(1, jeu);
-        }
-        
-        public void tictac() {
-			if (jeu.enCours()) {
-				if (decompte == 0) {
-					//if (choisirNiveau(f.ecouteurNiveau())) {
-					if(joueurs[joueurCourant].tempsEcoule())
-						changeJoueur();
-				} else {
-						//System.out.println("On vous attend, joueur " + joueurs[joueurCourant].num());
-						decompte = lenteurAttente;
-	                                        
-				}
-			} else {
-					decompte--;
-				}          
-		}
-            
+	/* public boolean choisirNiveau(String niveau){
+         if(!niv.equals(niveau)){
+             System.out.println("Vous avez choisit " + niveau);
+             niv=niveau;
+         }
+         switch(niveau){
+             case "Easy":return joueurs[joueurCourant].tempsEcoule();
+             case "Medium":return joueurs[joueurCourant].tempsEcouleNonPertant();
+             case "Hard": return joueurs[joueurCourant].tempsEcouleMinimax();
+             default:return false;
+         }
+     }*/
+	public void basculeIA(boolean value) {
+		jeuAutomatique = value;
+		System.out.println("jeuautomatique " + jeuAutomatique);
+		//f.changeBoutonIA(value);
+		if (jeuAutomatique)
+			joueurs[1] = new JoueurIA(1, jeu);
+		else
+			joueurs[1] = new JoueurHumain(1, jeu);
 	}
-        
+
+	public void tictac() {
+		if (jeu.enCours()) {
+			if (decompte == 0) {
+				//if (choisirNiveau(f.ecouteurNiveau())) {
+				if(joueurs[joueurCourant].tempsEcoule())
+					changeJoueur();
+			} else {
+				//System.out.println("On vous attend, joueur " + joueurs[joueurCourant].num());
+				decompte = lenteurAttente;
+
+			}
+		} else {
+			decompte--;
+		}
+	}
+
+}
+
 
