c2f13d387c675f4ff2a0e01720e9044b7e946a5c
wangxinwei <wangxinwei88900@gmail.com>
2019-05-14 12:13:40 +0000

    Update PlateauAffiche.java

--- src/Modele/PlateauAffiche.java
+++ src/Modele/PlateauAffiche.java
@@ -1,4 +1,4 @@
-package Modele;
+package Controleur.Modele;
 
 public class PlateauAffiche {
 	boolean[][] p;
@@ -6,63 +6,55 @@ public class PlateauAffiche {
 	public PlateauAffiche() {
 		p = new boolean[5][5];
 	}
-	
-	 public void initPlateauAffiche() {
+	public void initPlateauAffiche() {
 	        for(int i =0;i<p.length;i++) {
 	        	for(int j = 0;j<p[0].length;j++) {
 	        		p[i][j]=false;
 	        	}
 	        }
 	 }
-	 
-	 public void AffichePlateau() {
+	public void AffichePlateau() {
 		 System.out.println("Afficher PlateauAffiche");
 		 for(int i =0;i<p.length;i++) {
-	        	for(int j = 0;j<p[0].length;j++) {
-	        		if(p[i][j]==false)
-	        			System.out.print("0");
-	        		else
-	        			System.out.print("1");
-	        	}
-	        	System.out.println();
-	     }
+				for(int j = 0;j<p[0].length;j++) {
+					if(p[i][j]==false)
+						System.out.print("0");
+					else
+						System.out.print("1");
+				}
+				System.out.println();
+		 }
 	 }
-	 public void PlacerPiece(Piece piece) {
+	public void PlacerPiece(Piece piece) {
 		 for (int i = 0; i < 5; i++) {
-             for (int j = 0; j < 5; j++) {
-                 p[i][j] = piece.carres [i][j]; 
-             }
-         }
+			 for (int j = 0; j < 5; j++) {
+				 p[i][j] = piece.carres [i][j];
+			 }
+		 }
 	 }
-	 
-	 public boolean valeur(int i, int j) {
-	        return p[i][j];
+	public boolean valeur(int i, int j) {
+			return p[i][j];
 	 }
-	 
-	 public void setPiece(boolean [][] carres) {
+	public void setPiece(boolean [][] carres) {
 			this.p = carres;
 	 }
-	 
-	 public void retationGauche() {
-         boolean[][] carres = new boolean[5][5];
- 	       
-         for (int i = 0; i < 5; i++) {
-             for (int j = 0; j < 5; j++) {
-                 carres [i][j] = p[j][5-i-1];
-             }
-         }
-         setPiece(carres);	
-	 }
-	 
-	 public void Miroir() {
-		 
-		  boolean[][] carres = new boolean[5][5];
-	      for (int i = 0; i < 5; i++) {
-	            for (int j = 0; j < 5; j++) {
-	                carres[i][j] = p[i][5- j - 1];
-	            }
-	      }
-	       setPiece(carres);
-}
+	public void retationGauche() {
+		 boolean[][] carres = new boolean[5][5];
 
+		 for (int i = 0; i < 5; i++) {
+			 for (int j = 0; j < 5; j++) {
+				 carres [i][j] = p[j][5-i-1];
+			 }
+		 }
+		 setPiece(carres);
+	 }
+	public void Miroir() {
+		  boolean[][] carres = new boolean[5][5];
+		  for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					carres[i][j] = p[i][5- j - 1];
+				}
+		  }
+		  setPiece(carres);
+	}
 }
