90176d2e02b100523ebef359297857f62a31ba26
Tom Priore <azertkiller@gmail.com>
2019-05-16 12:37:42 +0000

    ArrayList to boolean array ...

--- src/Controleur/JoueurHumain.java
+++ src/Controleur/JoueurHumain.java
@@ -3,19 +3,19 @@ import Modele.Jeu;
 import Modele.Piece;
 import Modele.Position;
 
+import java.util.Random;
+
 public class JoueurHumain extends Joueur {
 	public JoueurHumain(int n, Jeu p) {
 		super(n, p);
 	}
 	boolean jeu(Position posPlateau,Position posPiece,Piece choix) {
-		if (jeu.placerPossible(posPlateau,posPiece,choix)) {
-			jeu.jouer(posPlateau,posPiece,choix);
-			jeu.piecesJ[jeu.joueurCourant].remove(choix.getNum());
+		if (jeu.placerPossible(posPlateau, posPiece, choix)) {
+			jeu.jouer(posPlateau, posPiece, choix);
+			jeu.piecesJ[jeu.joueurCourant][choix.getNum()] = false;
 			return true;
-		} else {
-			return false;
 		}
+
+		return false;
 	}
-	
-	
 }
