2b9c379241de9fb4dbc40928b730f0b0d6b348d0
Tom Priore <azertkiller@gmail.com>
2019-05-17 13:39:05 +0000

    Replace JoueurHumain.java

--- src/Controleur/JoueurHumain.java
+++ src/Controleur/JoueurHumain.java
@@ -11,11 +11,11 @@ public class JoueurHumain extends Joueur {
 	}
 	boolean jeu(Position posPlateau,Position posPiece,Piece choix) {
 		if (jeu.placerPossible(posPlateau, posPiece, choix)) {
+			jeu.piecesJ[jeu.joueurCourant].remove(jeu.pieces.get(choix.getNum()));
 			jeu.jouer(posPlateau, posPiece, choix);
-			jeu.piecesJ[jeu.joueurCourant][choix.getNum()] = false;
+
 			return true;
 		}
-
 		return false;
 	}
 }
