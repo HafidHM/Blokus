78f53298598d3400aa2e5aa446de0ef9d9c18b9c
wangxinwei <wangxinwei88900@gmail.com>
2019-05-21 14:41:59 +0000

    Update JoueurIA.java

--- src/Controleur/JoueurIA.java
+++ src/Controleur/JoueurIA.java
@@ -14,11 +14,35 @@ public class JoueurIA extends Joueur {
 		super(n, j);
 		r = new Random();
 	}
-
-	boolean tempsEcoule() {
+	boolean tempsEcoule(){
+		if(!jeu.jouable()){
+			jeu.changerJoueurSansJeuer();
+			return true;
+		}
+		else{
+			int bound = jeu.piecesJ[jeu.joueurCourant].size();
+			Position posPlateau;
+			boolean trouver=false;
+			while(!trouver){
+				num = r.nextInt(bound);
+				posPlateau = jeu.coord.get(r.nextInt(jeu.coord.size()));
+				jeu.setSelected(jeu.piecesJ[jeu.joueurCourant].get(num).getNum());
+				getFormePiece();
+				Piece choix = jeu.pieceCourant;
+				Position posPiece = getPosPiece(posPlateau, choix);
+				if (jeu.placerPossible(posPlateau, posPiece, choix)) {
+					jeu.piecesJ[jeu.joueurCourant].remove(jeu.pieces.get(choix.getNum()));
+					jeu.plateauPiece[jeu.joueurCourant].enlevePiece(choix.getNum());
+					jeu.jouer(posPlateau, posPiece, choix);
+					trouver = true;
+				}
+			}
+			return true;
+		}
+	}
+	boolean tempsEcouleeee() {
 		int bound;
 		Position posPlateau;
-
 		if(jeu.coord.size()>0) {
 			posPlateau = jeu.coord.get(r.nextInt(jeu.coord.size()));
 		}else{
@@ -26,56 +50,42 @@ public class JoueurIA extends Joueur {
 		}
 		if((bound = jeu.piecesJ[jeu.joueurCourant].size()) != 0) {
 			num = r.nextInt(bound);
-
 			jeu.setSelected(jeu.piecesJ[jeu.joueurCourant].get(num).getNum());
-
-
 			Piece choix = jeu.pieceCourant;
 			Position posPiece = getPosPiece(posPlateau, choix);
-
 			if (jeu.placerPossible(posPlateau, posPiece, choix)) {
 				jeu.piecesJ[jeu.joueurCourant].remove(jeu.pieces.get(choix.getNum()));
 				jeu.plateauPiece[jeu.joueurCourant].enlevePiece(choix.getNum());
 				jeu.jouer(posPlateau, posPiece, choix);
-
 				return true;
 			}
 		}
 		return false;
 	}
-
+	void getFormePiece(){
+		int tourner = r.nextInt(4);
+		int inverser = r.nextInt(2);
+		for(int i=0;i<=tourner;i++){
+			jeu.pieceCourant.retationGauche();
+		}
+		if(inverser==0){
+			jeu.pieceCourant.Miroir();
+		}
+	}
 	Position getPosPiece(Position posPl, Piece p) {
 		Position pos = new Position(0, 0);
-		int ok = 0;
-
-		if (ok < 4){
-			for (int i = 0; i < p.taille; i++) {
-				for (int j = 0; j < p.taille; j++) {
-					pos = new Position(i, j);
-					if (p.carres[i][j] == true) {
-						if (jeu.placerPossible(posPl, pos, p)) {
-							return pos;
-						}
+		for (int i = 0; i < p.taille; i++) {
+			for (int j = 0; j < p.taille; j++) {
+				pos = new Position(i, j);
+				if (p.carres[i][j]) {
+					if (jeu.placerPossible(posPl, pos, p)) {
+						return pos;
 					}
 				}
 			}
-
-		}else{
-			int tourner = r.nextInt(3);
-			int inverser = r.nextInt(2);
-			for (int i = 0; i < tourner; i++) {
-				p.retationDroite();
-			}
-			if(inverser == 0){
-				p.Miroir();
-			}
-			ok++;
 		}
-
-
 		return pos;
 	}
-
 	boolean tempsEcouleNonPerdant() {
 		int bound;
 		Position posPlateau;
@@ -99,13 +109,6 @@ public class JoueurIA extends Joueur {
 		}
 		return false;
 	}
-
-
-	/*Position nextPiece(Position posPl, Piece p){
-
-
-	}*/
-
 	Position closestToMiddle(){
 		Position closest = basePos();
 
@@ -127,7 +130,6 @@ public class JoueurIA extends Joueur {
 		return closest;
 
 	}
-
 	Position basePos(){
 		switch(jeu.joueurCourant){
 			case 0:
@@ -141,7 +143,6 @@ public class JoueurIA extends Joueur {
 		}
 		return new Position(0,0);
 	}
-
 	boolean tempsEcouleMinimax(){
 		int iandj [] = new int [2];
 
@@ -155,152 +156,4 @@ public class JoueurIA extends Joueur {
 		return true;
 
 	}
-/*
-	void nextStep(int[] iandj, int[][] p){
-
-		int Profondeur = MAXPROF;
-
-		int player = plateau.joueurCourant;
-
-		int values [] = Minimax(p,Profondeur, player);
-
-		iandj[0] = values[1];
-		iandj[1] = values[2];
-
-	}
-
-	int [] Minimax(int[][] p, int profondeur, int player){
-
-
-		//System.out.println("IN IA : " + profondeur);
-
-		List<int[]> allSteps = nextPossibleSteps(p);
-
-		int nextScore=0;
-
-		int finiteIJ [] = new int [2];
-		int finiteScore = 0;
-
-		if(profondeur == 0 || end(p)){
-			finiteScore = evaluation(p, player);
-		} else {
-			if (player == 0) {
-				int value = -10000;
-
-				for (int[] iandj : allSteps) {
-					//System.out.println("P0 "+iandj[0]+" "+iandj[1]);
-					int[][] config = nextConfig(p, iandj, OtherPlayer(player));
-					if (value < (nextScore = Minimax(config, profondeur - 1, OtherPlayer(player))[0]) ){
-						value = nextScore;
-						finiteIJ = iandj;
-					}
-				}
-				finiteScore =  value;
-
-			} else {
-				int value = 10000;
-
-				for (int[] iandj : allSteps) {
-					//System.out.println("P1 "+iandj[0]+" "+iandj[1]);
-					int[][] config = nextConfig(p, iandj, OtherPlayer(player));
-					if (value > (nextScore = Minimax(config, profondeur - 1, OtherPlayer(player))[0])) {
-						value = nextScore;
-						finiteIJ = iandj;
-					}
-				}
-				finiteScore = value;
-
-			}
-		}
-
-		return new int[] {finiteScore, finiteIJ[0], finiteIJ[1]};
-	}
-
-	List<int[]> nextPossibleSteps(int [][] p){
-		List<int[]> Steps = new ArrayList<int[]>();
-
-
-		if(end(p)){
-			return Steps;
-		}
-
-		for(int i=0; i<plateau.taille();i++){
-			for(int j=0; j<plateau.taille();j++){
-				if((p[i][j] == -1) && (!plateau.coupPoison(i, j))){
-					Steps.add(new int[] {i, j});
-				}
-			}
-		}
-
-		return Steps;
-
-	}
-
-	int [][] nextConfig(int[][] p, int [] iandj, int OPlayer) {
-		int[][] Config = new int[plateau.taille()][plateau.taille()];
-
-		for (int i = 0 ;i<plateau.taille();i++){
-			for(int j =0;j<plateau.taille();j++){
-				if(!plateau.coupPoison(i, j)) {
-					if ((i == iandj[0] && j == iandj[1]) && p[i][j] != 1) {
-						if (OPlayer == 0) {
-							Config[i][j] = 2;
-						} else {
-							Config[i][j] = 6;
-						}
-
-					} else if ((i >= iandj[0] && j >= iandj[1]) && p[i][j] != 1) {
-						if(p[i][j] == 4 || p[i][j] == 8){
-							Config[i][j] = 10;
-						}else {
-							if (OPlayer == 0) {
-								Config[i][j] = 4;
-							} else {
-								Config[i][j] = 8;
-							}
-						}
-
-					} else {
-						Config[i][j] = p[i][j];
-					}
-				}
-			}
-		}
-
-		return Config;
-
-
-
-	}
-
-	int evaluation(int [][] p, int player){
-		int score = 0;
-
-		if((player == 0) && (plateau.joueurCourant == 1)) {
-			score = -10;
-		} else if((player == 0) && (plateau.joueurCourant == 0)) {
-			score = 10;
-		} else if((player == 1) && (plateau.joueurCourant == 0)) {
-			score = -10;
-		} else if((player == 1) && (plateau.joueurCourant == 1)) {
-			score = 10;
-		}
-
-		return score;
-	}
-
-	int OtherPlayer(int player){
-		if(player == 0){
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-
-	boolean end(int[][] p) {
-		return ((p[0][1] != -1) && (p[1][0] != -1));
-
-	}
-*/
-
 }
