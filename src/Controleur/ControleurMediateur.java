08dd7bb14cf9c74c8b8c3718a2d869812ecf0d7f
biyun233 <syviass@outlook.com>
2019-05-23 14:08:45 +0000

    Replace ControleurMediateur.java

--- src/Controleur/ControleurMediateur.java
+++ src/Controleur/ControleurMediateur.java
@@ -1,4 +1,5 @@
 package Controleur;
+
 import Modele.Jeu;
 import Modele.Position;
 import Vue.ViewJouer;
@@ -8,145 +9,146 @@ import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextField;
 
 public class ControleurMediateur {
-	Jeu jeu;
-	ViewJouer vjouer;
-	ViewParametre vpara;
-	public int joueurCourant;
-	final int lenteurAttente = 50;
-	int decompte;
+    Jeu jeu;
+    ViewJouer vjouer;
+    ViewParametre vpara;
+    public int joueurCourant;
+    final int lenteurAttente = 50;
+    int decompte;
 
-	public ControleurMediateur(Jeu j, ViewJouer vj, ViewParametre vp) {
-		jeu = j;
-		vjouer = vj;
-		vpara = vp;
+    public ControleurMediateur(Jeu j, ViewJouer vj, ViewParametre vp) {
+        jeu = j;
+        vjouer = vj;
+        vpara = vp;
 
-	}
+    }
 
-	public void setDimension(int n) {
+    public void setDimension(int n) {
 		jeu = new Jeu(n);
 		vjouer.modify(jeu);
 		vpara.modify(jeu);
 		for(int i=0;i<4;i++) {
 			vpara.joueurs[i].modify(jeu);
 		}
-		System.out.println("view para " + vpara.plateau.taille());
 		vjouer.onLaunch();
-	}
-	public void modifScore(int nb) {
-		vjouer.Score.getChildren().clear();
-		if(nb == 4)
-			vjouer.Score.getChildren().addAll(vjouer.joueur_score[0],vjouer.joueur_score[1],vjouer.joueur_score[2],vjouer.joueur_score[3]);
-		else if(nb == 2)
-			vjouer.Score.getChildren().addAll(vjouer.joueur_score[0],vjouer.joueur_score[1]);
-	}
-	
-	public void setNom(TextField t,int order) {
-		t.textProperty().addListener(new ChangeListener<String>() {
-
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				t.setText(newValue);
-				vpara.nom[order] = newValue;
-			}
-		});
-	}
-
-	public void valide(int order) {
-		vjouer.joueur[order].setText(vpara.nom[order]);
-		vjouer.joueur_score[order].setText(vpara.nom[order]+ ": " + jeu.Score[order]);
-		if(vpara.nbJoueur==2) {
-			vjouer.joueur[order+2].setText(vpara.nom[order]);
-			vjouer.joueur_score[order].setText(vpara.nom[order]+ ": " + (jeu.Score[order]+jeu.Score[order+2]));
-		}
 
 	}
-	public void redimensionnement() {
-		vjouer.miseAJour();
-	}
 
-	public void clicSouris(double x, double y) {
-		int l = (int) (y / vjouer.hauteurCase());
-		int c = (int) (x / vjouer.largeurCase());
+    public void modifScore(int nb) {
+        vjouer.Score.getChildren().clear();
+        if(nb == 4)
+            vjouer.Score.getChildren().addAll(vjouer.joueur_score[0],vjouer.joueur_score[1],vjouer.joueur_score[2],vjouer.joueur_score[3]);
+        else if(nb == 2)
+            vjouer.Score.getChildren().addAll(vjouer.joueur_score[0],vjouer.joueur_score[1]);
+    }
 
-		System.out.println("lPlateau = " + l);
-		System.out.println("cPlateau = " + c);
-		Position posPlateau = new Position(l,c);
-		Position posPiece = new Position(jeu.PosPieceL,jeu.PosPieceC);
-		System.out.println("pieceCorant " + jeu.pieceCourant.getNum());
-		if (vpara.joueurs[joueurCourant].jeu(posPlateau,posPiece,jeu.pieceCourant)) {
-			jeu.plateauPiece[vjouer.joueurCourant].enlevePiece(jeu.pieceCourant.getNum());
-			vjouer.joueurCourant = jeu.joueurCourant;
-			vjouer.miseAJour();
-			changeJoueur();
+    public void setNom(TextField t,int order) {
+        t.textProperty().addListener(new ChangeListener<String>() {
 
-		}
-	}
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                t.setText(newValue);
+                vpara.nom[order] = newValue;
+            }
+        });
+    }
 
-	public void selectPiece(double x, double y) {
-		int l = (int) (y / vjouer.hauteurCasePiece());
-		int c = (int) (x / vjouer.largeurCasePiece());
-		if(jeu.plateauPiece[jeu.joueurCourant].valeur(l,c)>=0 && vjouer.joueurCourant==jeu.joueurCourant) {
-			jeu.setSelected(jeu.plateauPiece[jeu.joueurCourant].valeur(l,c));
-			jeu.plateauAffiche.PlacerPiece(jeu.pieceCourant);
-			vjouer.miseAJour();
-		}
-	}
+    public void valide(int order) {
+        vjouer.joueur[order].setText(vpara.nom[order]);
+        vjouer.joueur_score[order].setText(vpara.nom[order]+ ": " + jeu.Score[order]);
+        if(vpara.nbJoueur==2) {
+            vjouer.joueur[order+2].setText(vpara.nom[order]);
+            vjouer.joueur_score[order].setText(vpara.nom[order]+ ": " + (jeu.Score[order]+jeu.Score[order+2]));
+        }
 
-	public void PieceAffiche(double x, double y) {
-		int l = (int) (y / vjouer.hauteurCaseAffiche());
-		int c = (int) (x / vjouer.largeurCaseAffiche());
+    }
+    public void redimensionnement() {
+        vjouer.miseAJour();
+    }
 
-		System.out.println("lAffiche = " + l);
-		System.out.println("cAffiche = " + c);
-		jeu.setPieceL(l);
-		jeu.setPieceC(c);
-	}
+    public void clicSouris(double x, double y) {
+        int l = (int) (y / vjouer.hauteurCase());
+        int c = (int) (x / vjouer.largeurCase());
 
-	public void initAffiche() {
-		jeu.plateauAffiche.initPlateauAffiche();
-		vjouer.miseAJour();
-	}
-	void changeJoueur() {
-		joueurCourant = (joueurCourant + 1) % vpara.joueurs.length;
-		decompte = lenteurAttente;
-	}
+        System.out.println("lPlateau = " + l);
+        System.out.println("cPlateau = " + c);
+        Position posPlateau = new Position(l,c);
+        Position posPiece = new Position(jeu.PosPieceL,jeu.PosPieceC);
+        System.out.println("pieceCorant " + jeu.pieceCourant.getNum());
+        if (vpara.joueurs[joueurCourant].jeu(posPlateau,posPiece,jeu.pieceCourant)) {
+            jeu.plateauPiece[vjouer.joueurCourant].enlevePiece(jeu.pieceCourant.getNum());
+            vjouer.joueurCourant = jeu.joueurCourant;
+            vjouer.miseAJour();
+            changeJoueur();
+
+        }
+    }
+
+    public void selectPiece(double x, double y) {
+        int l = (int) (y / vjouer.hauteurCasePiece());
+        int c = (int) (x / vjouer.largeurCasePiece());
+        if(jeu.plateauPiece[jeu.joueurCourant].valeur(l,c)>=0 && vjouer.joueurCourant==jeu.joueurCourant) {
+            jeu.setSelected(jeu.plateauPiece[jeu.joueurCourant].valeur(l,c));
+            jeu.plateauAffiche.PlacerPiece(jeu.pieceCourant);
+            vjouer.miseAJour();
+        }
+    }
+
+    public void PieceAffiche(double x, double y) {
+        int l = (int) (y / vjouer.hauteurCaseAffiche());
+        int c = (int) (x / vjouer.largeurCaseAffiche());
+
+        System.out.println("lAffiche = " + l);
+        System.out.println("cAffiche = " + c);
+        jeu.setPieceL(l);
+        jeu.setPieceC(c);
+    }
+
+    public void initAffiche() {
+        jeu.plateauAffiche.initPlateauAffiche();
+        vjouer.miseAJour();
+    }
+    void changeJoueur() {
+        joueurCourant = (joueurCourant + 1) % vpara.joueurs.length;
+        decompte = lenteurAttente;
+    }
 
     public boolean choisirNiveau(String niveau){
-            switch(niveau){
-                case "Robot simple":return vpara.joueurs[joueurCourant].tempsEcoule();
-                case "Robot Intelligent":return vpara.joueurs[joueurCourant].tempsEcouleNonPerdant();
-                case "Robot Excellent": return vpara.joueurs[joueurCourant].tempsEcouleMinimax();
-                default:return false;
-            }
+        switch(niveau){
+            case "Robot simple": return vpara.joueurs[joueurCourant].tempsEcoule();
+            case "Robot Intelligent": return vpara.joueurs[joueurCourant].tempsEcouleNonPerdant();
+            case "Robot Excellent": return vpara.joueurs[joueurCourant].tempsEcouleMinimax();
+            default: return false;
+        }
     }
-    
-	public void tictac() {
-		boolean b;
-		if (jeu.enCours()) {
-			if (decompte == 0) {
-				if((b = choisirNiveau(vpara.dif[joueurCourant]))) {//num() pour joueurCourant change
-					System.out.println("joueurcorant " + joueurCourant + " " + b);
-					vjouer.joueurCourant = jeu.joueurCourant;
-					vjouer.miseAJour();
-					changeJoueur();
-				}
-				else {
-					if(vpara.joueurs[joueurCourant].jeu.enCoursJ[joueurCourant]) {
-						System.out.println("On vous attend, joueur " + joueurCourant);
-						decompte = lenteurAttente;
-					}
-					else{
-						vpara.joueurs[joueurCourant].jeu.setupNextJoueur();
-						vjouer.joueurCourant = jeu.joueurCourant;
-						vjouer.miseAJour();
-						changeJoueur();
-					}
 
-				}
-			}
-			else{
-				decompte--;
-			}
-		}
-	}
+    public void tictac() {
+        boolean b;
+
+        if (jeu.enCours()) {
+            if (decompte == 0) {
+                if((b = choisirNiveau(vpara.dif[joueurCourant]))) {//num() pour joueurCourant change
+                    vjouer.joueurCourant = jeu.joueurCourant;
+                    vjouer.miseAJour();
+                    changeJoueur();
+                }
+                else {
+                    if(vpara.joueurs[joueurCourant].jeu.enCoursJ[joueurCourant]) {
+                        System.out.println("On vous attend, joueur " + joueurCourant);
+                        decompte = lenteurAttente;
+                    }
+                    else{
+                        vpara.joueurs[joueurCourant].jeu.setupNextJoueur();
+                        vjouer.joueurCourant = jeu.joueurCourant;
+                        vjouer.miseAJour();
+                        changeJoueur();
+                    }
+
+                }
+            }
+            else{
+                decompte--;
+            }
+        }
+    }
 }
