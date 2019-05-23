80d99fd6506bef5698517d4b0d2590a1ed3bf91d
MOHAMED HAFID <hafid.mohamed.hm@gmail.com>
2019-05-23 00:25:50 +0000

    Update Historique.java

--- src/Modele/Historique.java
+++ src/Modele/Historique.java
@@ -1,24 +1,17 @@
 package Modele;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.Stack;
-
-import javafx.scene.control.Tab;
 
 public class Historique implements HistoriqueInterface{
     
            public Stack<Jeu> futur;
            public Stack<Jeu> passe;
-	       public Jeu mem;
+	   public Jeu mem;
+           
            String rep="historique/";
            String fichier;
 	
@@ -28,18 +21,8 @@ public class Historique implements HistoriqueInterface{
             mem = null;
             fichier=fic;
 	}
-
-        public void setMem(Jeu m) {
-            mem = m;
-            /* 
-                if(mem.getClass().getSimpleName().toString().compareTo("Jeu")==0) {
-					mem=(E)copy((Jeu)mem);
-		}
-             */
-        }
         
         
-	
 	@Override
 	public boolean peutAnnuler() {
 		return !passe.isEmpty();
@@ -51,13 +34,6 @@ public class Historique implements HistoriqueInterface{
 		return !futur.isEmpty();
 	}
         
-     public Jeu transfert(Stack<Jeu> source, Stack<Jeu> destination) {
-	    Jeu resultat = source.pop();
-		destination.push(resultat);
-		return resultat;
-	 }
-
-
 	@Override
 	public Jeu annuler() {
             if (peutAnnuler()) {
@@ -81,7 +57,54 @@ public class Historique implements HistoriqueInterface{
             return null;		
 	}
        
-      /*  
+ 
+        public void add(Jeu jeu){
+        	passe.push((Jeu)copyObject(jeu));
+        }
+        
+        public Jeu getNewJeu(){
+            return passe.peek();
+        }
+        
+        public Jeu transfert(Stack<Jeu> source, Stack<Jeu> destination) {
+	    Jeu resultat = source.pop();
+		destination.push(resultat);
+		return resultat;
+	}
+        
+        Object copyObject(Object src) {
+                Object dest = null;
+                try {
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    ObjectOutputStream oos = new ObjectOutputStream(bos);
+                    oos.writeObject(src);
+                    oos.flush();
+                    oos.close();
+                    bos.close();
+                    byte[] byteData = bos.toByteArray();
+                    ByteArrayInputStream bais = new ByteArrayInputStream(byteData);
+                    try {
+                        dest = new ObjectInputStream(bais).readObject();
+                    } catch (ClassNotFoundException e) {
+                        System.err.println("Class non trouvee");
+                    }
+                } catch (IOException e) {
+                    System.err.println("Erreur dentree sortie");
+                }
+            return dest;
+        }
+        
+        public void setMem(Jeu m) {
+            mem = m;
+            /* 
+                if(mem.getClass().getSimpleName().toString().compareTo("Jeu")==0) {
+					mem=(E)copy((Jeu)mem);
+		}
+             */
+        }
+        
+        
+             /*  
         public  void save(){
                 FileOutputStream fileOut;
                 try {
@@ -120,32 +143,6 @@ public class Historique implements HistoriqueInterface{
                 return obj;
         }
        */ 
-        public void add(Jeu jeu){
-        	passe.push((Jeu)copyObject(jeu));
-        }
-        
-        private Object copyObject(Object objSource) {
-                Object objDest = null;
-                try {
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    ObjectOutputStream oos = new ObjectOutputStream(bos);
-
-                    oos.writeObject(objSource);
-                    oos.flush();
-                    oos.close();
-                    bos.close();
-                    byte[] byteData = bos.toByteArray();
-                    ByteArrayInputStream bais = new ByteArrayInputStream(byteData);
-                    try {
-                        objDest = new ObjectInputStream(bais).readObject();
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            return objDest;
-        }
      
            
 }
